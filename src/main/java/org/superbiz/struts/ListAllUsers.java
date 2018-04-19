d9b65f32d72ab4b742be886f31ba1c737855fc10
srirampvvm <plnvbs@gmail.com>
2018-04-19 10:58:56 -0500

    struts to spring boot

--- src/main/java/org/superbiz/struts/ListAllUsers.java
+++ src/main/java/org/superbiz/struts/ListAllUsers.java
@@ -17,16 +17,25 @@
  */
 package org.superbiz.struts;
 
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import java.util.List;
 import java.util.Properties;
 
+@Component
 public class ListAllUsers {
 
     private int id;
     private String errorMessage;
     private List<User> users;
+    private UserService userService;
+
+    public ListAllUsers(UserService userService) {
+        this.userService = userService;
+    }
 
     public List<User> getUsers() {
         return users;
@@ -52,16 +61,17 @@ public class ListAllUsers {
         this.id = id;
     }
 
+    @Transactional
     public String execute() {
 
         try {
-            UserService service = null;
+            /*UserService service = null;
             Properties props = new Properties();
             props.put(Context.INITIAL_CONTEXT_FACTORY,
                 "org.apache.openejb.core.LocalInitialContextFactory");
             Context ctx = new InitialContext(props);
-            service = (UserService) ctx.lookup("UserServiceImplLocal");
-            this.users = service.findAll();
+            service = (UserService) ctx.lookup("UserServiceImplLocal");*/
+            this.users = userService.findAll();
         } catch (Exception e) {
             this.errorMessage = e.getMessage();
             return "failure";
